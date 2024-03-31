e83cd66faf2bccabe77e1beae6f5f6b1848855c5
tommypatra <tommyirawan.patra@gmail.com>
2024-03-31 13:32:52 +0800

    update loading.js

--- public/js/loading.js
+++ public/js/loading.js
@@ -1,35 +1,25 @@
-function renderPagination(response, paginationElement) {
-    var pagination = paginationElement;
+var myModalWeb = new bootstrap.Modal(document.getElementById('loadingModal'), {
+    keyboard: false
+});       
 
-    pagination.empty();
-    if (response.last_page > 1) {
-        var currentPage = response.current_page;
-        var lastPage = response.last_page;
-
-        var startPage = Math.max(1, currentPage - 2);
-        var endPage = Math.min(lastPage, currentPage + 2);
-
-        if (startPage > 1) {
-            pagination.append('<li class="page-item"><a class="page-link" href="javascript:;" data-page="1">1</a></li>');
-            if (startPage > 2) {
-                pagination.append('<li class="page-item"><span class="page-link">...</span></li>');
-            }
-        }
-
-        for (var i = startPage; i <= endPage; i++) {
-            var activeClass = i === currentPage ? 'active' : '';
-            pagination.append('<li class="page-item ' + activeClass + '"><a class="page-link" href="javascript:;" data-page="' + i + '">' + i + '</a></li>');
-        }
-
-        if (endPage < lastPage) {
-            if (endPage < lastPage - 1) {
-                pagination.append('<li class="page-item"><span class="page-link">...</span></li>');
-            }
-            pagination.append('<li class="page-item"><a class="page-link" href="javascript:;" data-page="' + lastPage + '">' + lastPage + '</a></li>');
-        }
-        
-        // Menampilkan jumlah data
-        var totalData = response.total;
-        pagination.append('<li class="page-item disabled"><span class="page-link">Total Data: ' + totalData + '</span></li>');
-    }
+function showLoading() {
+    $('.proses-berjalan').html('');
+    myModalWeb.show();
 }
+
+function hideLoading() {
+    $(document).on('shown.bs.modal', '#loadingModal', function () {
+        var modalElement = document.getElementById('loadingModal');
+        var mymodal = bootstrap.Modal.getInstance(modalElement);
+        mymodal.hide();
+    });
+    $('.proses-berjalan').html('');
+}
+
+$(document).ajaxStart(function() {
+    showLoading();
+});
+
+$(document).ajaxStop(function() {
+    hideLoading();
+});
