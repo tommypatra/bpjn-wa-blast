7b48d4b8fdcfe1d5af6a3e2a8ec09c2a1af8a901
tommypatra <tommyirawan.patra@gmail.com>
2024-05-25 22:01:25 +0800

    disable show loading

--- public/js/loading.js
+++ public/js/loading.js
@@ -1,28 +1,28 @@
-var modalElement = document.getElementById('loadingModal');
-var modal = new bootstrap.Modal(modalElement, {
-    keyboard: false
-});
+// var modalElement = document.getElementById('loadingModal');
+// var modal = new bootstrap.Modal(modalElement, {
+//     keyboard: false
+// });
 
-function showLoading() {
-    $('.proses-berjalan').html('');
-    modal.show();
-}
+// function showLoading() {
+//     $('.proses-berjalan').html('');
+//     modal.show();
+// }
 
-function hideLoading() {
-    modalElement.addEventListener('shown.bs.modal', function () {
-        modal.hide();
-    });    
-    $('.proses-berjalan').html('');
-}
+// function hideLoading() {
+//     modalElement.addEventListener('shown.bs.modal', function () {
+//         modal.hide();
+//     });    
+//     $('.proses-berjalan').html('');
+// }
 
-$(document).ajaxStart(function() {
-    //showLoading();
-});
+// $(document).ajaxStart(function() {
+//     showLoading();
+// });
 
-$(document).ajaxStop(function() {
-    //hideLoading();
-});
+// $(document).ajaxStop(function() {
+//     hideLoading();
+// });
 
-$(document).ajaxError(function() {
-    //hideLoading();
-});
+// $(document).ajaxError(function() {
+//     hideLoading();
+// });
